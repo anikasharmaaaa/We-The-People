b271cfede624e6adffc3d6cf81d6a890254b8a98
Anika Sharma <aniku.sharma@gmail.com>
2020-08-12 11:20:47 -0700

    Initial Commit

---
+++ We The People/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  We The People
+//
+//  Created by Anika Sharma on 8/12/20.
+//  Copyright © 2020 Anika Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
