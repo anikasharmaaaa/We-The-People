dcf49d9e2b26dd0006e393f8df5a69356d4487c4
Khuffman75 <Khuffman0905@gmail.com>
2020-08-13 14:28:12 -0700

    Edit #3

---
+++ We The People/MyProtestTableViewController.swift
@@ -0,0 +1,72 @@
+//
+//  MyProtestTableViewController.swift
+//  We The People
+//
+//  Created by SVD on 8/13/20.
+//  Copyright © 2020 Anika Sharma. All rights reserved.
+//
+
+import UIKit
+
+class MyProtestTableViewController: UITableViewController, UISearchBarDelegate {
+
+    let data = ["Thursday - August 13", "Friday - August 14", "Saturday - August 15", "Sunday - August 16"]
+    var filteredData: [String]!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchBar.delegate = self
+        
+        filteredData = data
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        
+        return filteredData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+        
+        cell.textLabel?.text = filteredData[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    //MARK: Search Bar Config
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        filteredData = []
+        
+        if searchText == "" {
+            
+            filteredData = data
+        }
+        else {
+            for protest in data {
+            
+                if protest.lowercased().contains(searchText.lowercased()) {
+                
+                    filteredData.append(protest)
+                }
+            }
+        }
+        
+        self.tableView.reloadData()
+    }
+}
